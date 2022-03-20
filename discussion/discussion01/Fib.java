23f5721e9d44661831eee471277163aec6d1c918
littlekimber <zhyan0096@gmail.com>
2022-03-20 13:27:53 -0700

    cs61b

---
+++ discussion/discussion01/Fib.java
@@ -0,0 +1,19 @@
+package discussion01;
+
+public class Fib {
+
+        public static int fib(int n){
+            if(n==1){
+                return 0;
+            }else if(n==2){
+                return 1;
+            }
+            return fib(n-1)+fib(n-2);
+        }
+
+        public static void main(String[] args){
+            System.out.println(fib(5));
+        }
+}
+
+
---
+++ lab/lab1/HelloNumber.java
@@ -0,0 +1,9 @@
+public class HelloNumber{
+	public static void main(String[] args){
+		int x = 0;
+		for(int i=0; i<=10;i++){
+			System.out.println(x);
+			x += i;
+		}
+	}
+}
